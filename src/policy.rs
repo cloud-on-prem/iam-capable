8968066ffcd7d53ea65839c12a2971485ef62d42
Prem Pillai <prem@squareup.com>
2023-03-16 21:28:20 +1100

    Refactored Policy.rs

--- src/policy.rs
+++ src/policy.rs
@@ -2,11 +2,43 @@ use aws_sdk_iam::Client as IamClient;
 use serde_json::Value;
 use url::form_urlencoded;
 
+/// Fetches the policy statements for a given IAM Role.
+///
+/// # Arguments
+///
+/// * `iam` - A reference to an IamClient instance.
+/// * `account_id` - A string slice containing the AWS account ID.
+/// * `role_name` - A string slice containing the IAM Role name.
+///
+/// # Returns
+///
+/// A Result containing a Vec of Value representing the policy statements, or an Error.
 pub async fn fetch_role_policy(
     iam: &IamClient,
     account_id: &str,
     role_name: &str,
 ) -> Result<Vec<Value>, Box<dyn std::error::Error + Send + Sync>> {
+    let attached_policy_arns = fetch_attached_policy_arns(iam, role_name).await?;
+    let inline_policy_arns = fetch_inline_policy_arns(iam, account_id, role_name).await?;
+
+    let policy_arns = attached_policy_arns
+        .into_iter()
+        .chain(inline_policy_arns.into_iter());
+
+    let mut policies = Vec::<Value>::new();
+
+    for policy_arn in policy_arns {
+        let json_document = fetch_json_policy_document(iam, &policy_arn).await?;
+        policies.push(json_document);
+    }
+
+    Ok(policies)
+}
+
+async fn fetch_attached_policy_arns(
+    iam: &IamClient,
+    role_name: &str,
+) -> Result<Vec<String>, Box<dyn std::error::Error + Send + Sync>> {
     let attached_policies = iam
         .list_attached_role_policies()
         .role_name(role_name)
@@ -20,7 +52,16 @@ pub async fn fetch_role_policy(
         .filter_map(|policy| policy.policy_arn)
         .collect();
 
+    Ok(policy_arns)
+}
+
+async fn fetch_inline_policy_arns(
+    iam: &IamClient,
+    account_id: &str,
+    role_name: &str,
+) -> Result<Vec<String>, Box<dyn std::error::Error + Send + Sync>> {
     let inline_policies = iam.list_role_policies().role_name(role_name).send().await?;
+
     let inline_policy_arns: Vec<String> = inline_policies
         .policy_names
         .unwrap_or_default()
@@ -32,30 +73,35 @@ pub async fn fetch_role_policy(
             )
         })
         .collect();
-    // let policy_names: Vec<String> = policy_arns
-    //     .into_iter()
-    //     .chain(inline_policies.policy_names.unwrap_or_default().into_iter())
-    //     .collect();
-    let policy_arns = policy_arns
-        .into_iter()
-        .chain(inline_policy_arns.into_iter());
 
-    let mut policies = Vec::<Value>::new();
-
-    for policy_arn in policy_arns {
-        let document = fetch_policy_document(&iam, &policy_arn).await?;
-
-        let decoded_document: String = form_urlencoded::parse(document.as_bytes())
-            .map(|(key, _)| key)
-            .collect();
-
-        let json_document: Value = serde_json::from_str(&decoded_document).unwrap();
-        policies.push(json_document);
-    }
-
-    Ok(policies)
+    Ok(inline_policy_arns)
 }
 
+async fn fetch_json_policy_document(
+    iam: &IamClient,
+    policy_arn: &str,
+) -> Result<Value, Box<dyn std::error::Error + Send + Sync>> {
+    let document = fetch_policy_document(iam, policy_arn).await?;
+
+    let decoded_document: String = form_urlencoded::parse(document.as_bytes())
+        .map(|(key, _)| key)
+        .collect();
+
+    let json_document: Value = serde_json::from_str(&decoded_document).unwrap();
+
+    Ok(json_document)
+}
+
+/// Fetches the policy document for a given policy ARN, focusing on the default policy version.
+///
+/// # Arguments
+///
+/// * `iam` - A reference to an IamClient instance.
+/// * `policy_arn` - A string slice containing the policy ARN.
+///
+/// # Returns
+///
+/// A Result containing a String representing the policy document, or an Error.
 async fn fetch_policy_document(
     iam: &IamClient,
     policy_arn: &str,
@@ -68,7 +114,6 @@ async fn fetch_policy_document(
 
     let policy_versions = policy_versions_output.versions.unwrap_or_default();
 
-    // Filter policy versions to get the default one
     let default_policy_version = policy_versions
         .iter()
         .find(|version| version.is_default_version);
