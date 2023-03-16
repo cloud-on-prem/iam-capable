3c6d3c9122e2fc1292a26bdc6649c267785e9137
Prem Pillai <prem@squareup.com>
2023-03-16 22:17:56 +1100

    Refactor Compare.rs

--- src/compare.rs
+++ src/compare.rs
@@ -1,17 +1,30 @@
-// use colored::*;
-use cli_table::{format::Justify, Cell, Table};
-use cli_table::{print_stdout, Style};
+use cli_table::{format::Justify, print_stdout, Table, WithTitle};
 use serde_json::Value;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::iter::FromIterator;
 
+/// Represents an AWS capability, consisting of a resource and an action.
 #[derive(Hash, PartialEq, Eq, Debug)]
 struct Capability {
     resource: String,
     action: String,
 }
 
+/// Represents a row in the comparison table.
+#[derive(Table)]
+struct CapabilityRow {
+    #[table(title = "Resource", justify = "Justify::Left")]
+    resource: String,
+    #[table(title = "Action", justify = "Justify::Left")]
+    action: String,
+    #[table(title = "Role 1", justify = "Justify::Center")]
+    has_capability1: String,
+    #[table(title = "Role 2", justify = "Justify::Center")]
+    has_capability2: String,
+}
+
+/// Compares two sets of policies and outputs a table displaying their differences.
 pub fn compare_policies(policies1: &[Value], policies2: &[Value], role1: &str, role2: &str) {
     let mut capabilities1 = HashMap::<Capability, bool>::new();
     let mut capabilities2 = HashMap::<Capability, bool>::new();
@@ -30,9 +43,41 @@ pub fn compare_policies(policies1: &[Value], policies2: &[Value], role1: &str, r
         capabilities2.extend(policy_capabilities2);
     }
 
-    display_comparison_table(&capabilities1, &capabilities2, role1, role2);
+    let all_keys_set: HashSet<&Capability> =
+        HashSet::from_iter(capabilities1.keys().chain(capabilities2.keys()));
+    let mut all_keys: Vec<&Capability> = all_keys_set.into_iter().collect();
+    all_keys.sort_by(|a, b| {
+        let res = a.resource.cmp(&b.resource);
+        if res == std::cmp::Ordering::Equal {
+            a.action.cmp(&b.action)
+        } else {
+            res
+        }
+    });
+
+    let capability_rows: Vec<CapabilityRow> = all_keys
+        .into_iter()
+        .filter_map(|key| {
+            let has_capability1 = capabilities1.get(key).unwrap_or(&false);
+            let has_capability2 = capabilities2.get(key).unwrap_or(&false);
+
+            if !(*has_capability1 && *has_capability2) {
+                Some(CapabilityRow {
+                    resource: key.resource.clone(),
+                    action: key.action.clone(),
+                    has_capability1: has_capability1.to_string(),
+                    has_capability2: has_capability2.to_string(),
+                })
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    display_table(capability_rows);
 }
 
+/// Extracts the capabilities from the policy statements.
 fn extract_capabilities(statements: &[Value]) -> HashMap<Capability, bool> {
     let mut capabilities: HashMap<Capability, bool> = HashMap::new();
 
@@ -50,7 +95,6 @@ fn extract_capabilities(statements: &[Value]) -> HashMap<Capability, bool> {
             .iter()
             .filter_map(|v| v.as_str().map(String::from))
             .collect::<Vec<_>>();
-
         for action in &actions {
             for resource in &resources {
                 let capability = Capability {
@@ -61,56 +105,11 @@ fn extract_capabilities(statements: &[Value]) -> HashMap<Capability, bool> {
             }
         }
     }
-
     capabilities
 }
 
-fn display_comparison_table(
-    capabilities1: &HashMap<Capability, bool>,
-    capabilities2: &HashMap<Capability, bool>,
-    role1: &str,
-    role2: &str,
-) {
-    let all_keys_set: HashSet<&Capability> =
-        HashSet::from_iter(capabilities1.keys().chain(capabilities2.keys()));
-    let mut all_keys: Vec<&Capability> = all_keys_set.into_iter().collect();
-    all_keys.sort_by(|a, b| {
-        let res = a.resource.cmp(&b.resource);
-        if res == std::cmp::Ordering::Equal {
-            a.action.cmp(&b.action)
-        } else {
-            res
-        }
-    });
-
-    let rows: Vec<Vec<_>> = all_keys
-        .into_iter()
-        .filter_map(|key| {
-            let has_capability1 = capabilities1.get(key).unwrap_or(&false);
-            let has_capability2 = capabilities2.get(key).unwrap_or(&false);
-
-            if !(*has_capability1 && *has_capability2) {
-                Some(vec![
-                    key.resource.clone().cell(),
-                    key.action.clone().cell(),
-                    has_capability1.cell().justify(Justify::Center),
-                    has_capability2.cell().justify(Justify::Center),
-                ])
-            } else {
-                None
-            }
-        })
-        .collect();
-
-    let table = rows
-        .table()
-        .title(vec![
-            "Resource".cell().bold(true).justify(Justify::Center),
-            "Action".cell().bold(true).justify(Justify::Center),
-            role1.cell().bold(true).justify(Justify::Center),
-            role2.cell().bold(true).justify(Justify::Center),
-        ])
-        .bold(true);
-
-    assert!(print_stdout(table).is_ok());
+/// Displays the comparison table for two sets of policies.
+fn display_table(capability_rows: Vec<CapabilityRow>) {
+    let table = capability_rows.with_title();
+    print_stdout(table).unwrap();
 }
