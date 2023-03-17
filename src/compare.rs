b51786ecc0469e072cd2d44c2043f373b4e16dde
Prem Pillai <prem@squareup.com>
2023-03-17 20:58:12 +1100

    Add tests for compare

--- src/compare.rs
+++ src/compare.rs
@@ -91,3 +91,76 @@ fn extract_capabilities(statements: Vec<PolicyStatement>) -> HashMap<Capability,
     }
     capabilities
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn make_policy_statement(actions: Vec<&str>, resources: Vec<&str>) -> PolicyStatement {
+        PolicyStatement {
+            action: actions.into_iter().map(String::from).collect(),
+            resource: resources.into_iter().map(String::from).collect(),
+            effect: "Allow".to_string(),
+        }
+    }
+
+    #[test]
+    fn test_extract_capabilities() {
+        let statements = vec![
+            make_policy_statement(vec!["s3:ListBucket"], vec!["arn:aws:s3:::mybucket"]),
+            make_policy_statement(vec!["s3:GetObject"], vec!["arn:aws:s3:::mybucket/*"]),
+        ];
+
+        let capabilities = extract_capabilities(statements);
+
+        assert_eq!(capabilities.len(), 2);
+        assert_eq!(
+            capabilities[&Capability {
+                resource: "arn:aws:s3:::mybucket".to_string(),
+                action: "s3:ListBucket".to_string(),
+            }],
+            true
+        );
+        assert_eq!(
+            capabilities[&Capability {
+                resource: "arn:aws:s3:::mybucket/*".to_string(),
+                action: "s3:GetObject".to_string(),
+            }],
+            true
+        );
+    }
+
+    fn make_policy(statements: Vec<PolicyStatement>) -> Policy {
+        Policy {
+            statement: statements,
+            version: "2012-10-17".to_string(),
+        }
+    }
+
+    #[test]
+    fn test_compare_policies() {
+        let policies1 = vec![make_policy(vec![make_policy_statement(
+            vec!["s3:ListBucket"],
+            vec!["arn:aws:s3:::mybucket"],
+        )])];
+
+        let policies2 = vec![make_policy(vec![make_policy_statement(
+            vec!["s3:GetObject"],
+            vec!["arn:aws:s3:::mybucket/*"],
+        )])];
+
+        let capability_rows = compare_policies(policies1, policies2);
+
+        assert_eq!(capability_rows.len(), 2);
+
+        assert_eq!(capability_rows[0].resource, "arn:aws:s3:::mybucket");
+        assert_eq!(capability_rows[0].action, "s3:ListBucket");
+        assert_eq!(capability_rows[0].has_capability1, true);
+        assert_eq!(capability_rows[0].has_capability2, false);
+
+        assert_eq!(capability_rows[1].resource, "arn:aws:s3:::mybucket/*");
+        assert_eq!(capability_rows[1].action, "s3:GetObject");
+        assert_eq!(capability_rows[1].has_capability1, false);
+        assert_eq!(capability_rows[1].has_capability2, true);
+    }
+}
