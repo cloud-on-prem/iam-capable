a6cbf916969a1d8e3065159a85fad8f528fd7ffc
Prem Pillai <prem.pillai@gmail.com>
2023-03-18 13:16:05 +1100

    Tests for Output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use capability::{CapabilityRow, extract_capabilities_from_policies};
 use aws::client::get_aws_client;
+use std::io::stdout;
 
 mod aws;
 mod compare;
@@ -13,6 +14,7 @@ mod capability;
 async fn main() {
     let args = cli::fetch_cli_args();
     let aws_client = get_aws_client().await.unwrap();
+    let mut writer = stdout();
 
     match args {
         cli::IamCapable::Compare {
@@ -28,14 +30,14 @@ async fn main() {
                 .unwrap();
             let rows: Vec<CapabilityRow> = compare::compare_policies(policy1, policy2);
 
-            output::format::print(output_format, &rows)
+            output::format::print(output_format, &rows, &mut writer)
         }
         cli::IamCapable::Fetch { role, output_format } => {
             // Fetch the policies for the single role
             let policies = policy::fetch_role_policy(&aws_client.iam, &aws_client.account_id, &role).await.unwrap();
 
             let rows = extract_capabilities_from_policies(policies);
-            output::format::print(output_format, &rows);
+            output::format::print(output_format, &rows, &mut writer);
         }
     }
 }
--- src/output/format.rs
+++ src/output/format.rs
@@ -1,5 +1,5 @@
 use std::str::FromStr;
-use std::io::stdout;
+use std::io::Write;
 
 use super::csv::{write_csv};
 use super::json::write_json;
@@ -29,19 +29,101 @@ impl FromStr for OutputFormat {
     }
 }
 
-pub fn print<T: OutputSerializable>(output_format: OutputFormat, data: &[T]) {
+pub fn print<T: OutputSerializable, W: Write>(
+    output_format: OutputFormat,
+    data: &[T],
+    writer: &mut W,
+) {
     match output_format {
         OutputFormat::Csv => {
             // Output as CSV
-            if let Err(e) = write_csv(data, stdout()) {
+            if let Err(e) = write_csv(data, writer) {
                 eprintln!("Error writing CSV: {}", e);
             }
         }
         OutputFormat::Json => {
             // Output as JSON
-            if let Err(e) = write_json(data, stdout()) {
+            if let Err(e) = write_json(data, writer) {
                 eprintln!("Error writing JSON: {}", e);
             }
         }
     }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+    use super::{OutputSerializable, print, OutputFormat};
+
+    // Dummy struct for testing purposes
+    #[derive(Debug, PartialEq)]
+    struct TestData {
+        value1: String,
+        value2: i32,
+    }
+
+    impl OutputSerializable for TestData {
+        fn csv_header() -> Vec<&'static str> {
+            vec!["value1", "value2"]
+        }
+
+        fn csv_record(&self) -> Vec<String> {
+            vec![self.value1.clone(), self.value2.to_string()]
+        }
+
+        fn to_json_value(&self) -> serde_json::Value {
+            serde_json::json!({
+                "value1": self.value1,
+                "value2": self.value2,
+            })
+        }
+    }
+
+    #[test]
+    fn test_print_csv() {
+        let data = vec![
+            TestData {
+                value1: "test1".to_string(),
+                value2: 42,
+            },
+            TestData {
+                value1: "test2".to_string(),
+                value2: 99,
+            },
+        ];
+
+        let mut cursor = Cursor::new(Vec::new());
+        print(OutputFormat::Csv, &data, &mut cursor);
+
+        let csv_output = String::from_utf8(cursor.into_inner()).unwrap();
+        assert_eq!(csv_output, "value1,value2\ntest1,42\ntest2,99\n");
+    }
+
+    #[test]
+    fn test_print_json() {
+        let data = vec![
+            TestData {
+                value1: "test1".to_string(),
+                value2: 42,
+            },
+            TestData {
+                value1: "test2".to_string(),
+                value2: 99,
+            },
+        ];
+
+        let mut cursor = Cursor::new(Vec::new());
+        print(OutputFormat::Json, &data, &mut cursor);
+
+        let json_output = String::from_utf8(cursor.into_inner()).unwrap();
+        let parsed_json: serde_json::Value = serde_json::from_str(&json_output).unwrap();
+        let expected_json: serde_json::Value = serde_json::json!([
+            {"value1": "test1", "value2": 42},
+            {"value1": "test2", "value2": 99},
+        ]);
+
+        assert_eq!(parsed_json, expected_json);
+    }
+
 }
